5c020faed5378aae7ecadfa19d20a1c98db5adcf
gastonfvargas <gastonfvargas@gmail.com>
2024-07-07 04:03:34 -0300

    Se suben cambios para resolver algunos errores en la pagina

--- src/controllers/authentication/registro2.controller.js
+++ src/controllers/authentication/registro2.controller.js
@@ -1,3 +1,20 @@
-module.exports = (req, res) =>{
-    res.render("./authentication/registro-2");
-}
+const db = require('../../dataBase/models');
+
+module.exports = (req, res) => {
+    db.user.findOne({
+        order: [['id', 'DESC']] // Ordenar por ID en orden descendente
+    })
+    .then((u) => {
+        if (!u) {
+            return res.status(404).send('No se encontraron usuarios');
+        }
+        
+        // Renderizar la vista y pasar el objeto `user`
+        res.render("authentication/registro-2", { user: u });
+    })
+    .catch((error) => {
+        console.error('Error al buscar usuario:', error);
+        res.status(500).send('Error interno del servidor');
+    });
+
+};
