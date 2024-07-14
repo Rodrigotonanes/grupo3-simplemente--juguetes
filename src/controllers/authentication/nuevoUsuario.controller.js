52653ac0d4b1703c964a95d94bdc2d09da44affc
gastonfvargas <gastonfvargas@gmail.com>
2024-07-14 01:58:25 -0300

    Se soluciona el error de agregar nuevos usuarios

--- src/controllers/authentication/nuevoUsuario.controller.js
+++ src/controllers/authentication/nuevoUsuario.controller.js
@@ -1,59 +1,55 @@
-// const { loadData, saveData } = require("../../dataBase");
-const db = require('../../dataBase/models')
+const db = require('../../dataBase/models');
 const { validationResult } = require("express-validator");
-const bc = require('bcryptjs')
-module.exports = (req,res)=>{
-    const errors = validationResult(req)
-    // const {userName} = req.body
+const bc = require('bcryptjs');
 
-    // db.user.findOne({
-    //     where: {userName}
-    // }).then((u)=>{
-    //     if(!u){
-    //         return res.render("./authentication/errorAuth",
-    //         {mensage: "El usuario ya existe",
-    //         boton: "Intente nuevamente", 
-    //         redireccion: "/autenticacion/registro"
-    //         });
-    //     }
+module.exports = (req, res) => {
+    const errors = validationResult(req);
 
+    if (errors.isEmpty()) {
+        // Extraer datos del formulario
+        const { userName, name, email, password, province, locality, postal, street, streetNumber, floor, betweenSt1, betweenSt2, phoneNumber, indications } = req.body;
         
-    // })
-    if(errors.isEmpty()){
-    const {userName,name,email,password,province,locality,postal,street,streetNumber,floor,betweenSt1,betweenSt2,phoneNumber,indications,} = req.body
- 
-    // const usuarios = loadData("usuarios");
-    // const nuevaid = usuarios[usuarios.length - 1].id + 1;
-    db.user.create( {
-        userName: userName.trim(),
-        name: name?name.trim():null,
-        email: email.trim(),
-        password: bc.hashSync(password.trim(),10),
-        province: province?+province:null,
-        locality: locality?locality.trim():null,
-        postal: postal? +postal: null,
-        street: street?street.trim():null,
-        streetNumber: streetNumber? +streetNumber: null,
-        floor: floor? +floor: null,
-        betweenSt1: betweenSt1?betweenSt1.trim():null,
-        betweenSt2: betweenSt2?betweenSt2.trim():null,
-        phoneNumber: phoneNumber? +phoneNumber: null,
-        indications: indications?indications.trim():null,
-        userPicture: req.files.userPicture?.length? req.files.userPicture[0]?.filename : "default.png",
-        role: 1
-    })
-  
-    // usuarios.push(nuevoUsuario);
-    // saveData(usuarios,"usuarios")
-    res.redirect("/autenticacion/registro-completado")
-}else{
-    const mapeoErroes = errors.mapped()
-    // const errores = [mapeoErroes.userName,mapeoErroes.name,mapeoErroes.email,mapeoErroes.password,mapeoErroes.confirmpassword,mapeoErroes.province,mapeoErroes.locality,mapeoErroes.postal,mapeoErroes.street,mapeoErroes.streetNumber,mapeoErroes.floor,mapeoErroes.phoneNumber,mapeoErroes.userPicture]
-    const old = req.body
-    // return res.send(mapeoErroes)
-    res.render("./authentication/registro",{
-        errors: mapeoErroes,
-        old: old,
-    })
-}
-}
+        // Crear nuevo usuario en la base de datos
+        db.user.create({
+            userName: userName.trim(),
+            name: name ? name.trim() : null,
+            email: email.trim(),
+            password: bc.hashSync(password.trim(), 10),
+            province: province ? +province : null,
+            locality: locality ? locality.trim() : null,
+            postal: postal ? +postal : null,
+            street: street ? street.trim() : null,
+            streetNumber: streetNumber ? +streetNumber : null,
+            floor: floor ? +floor : null,
+            betweenSt1: betweenSt1 ? betweenSt1.trim() : null,
+            betweenSt2: betweenSt2 ? betweenSt2.trim() : null,
+            phoneNumber: phoneNumber ? +phoneNumber : null,
+            indications: indications ? indications.trim() : null,
+            userPicture: req.files.userPicture?.length ? req.files.userPicture[0]?.filename : "default.png",
+            role: 1
+        })
+        .then(() => {
+            // Redirigir después de un registro exitoso
+            res.redirect("/autenticacion/registro-completado");
+        })
+        .catch(error => {
+            // Manejo de errores en la base de datos
+            console.error(error);
+            res.render("./authentication/errorAuth", {
+                message: "Error al registrar el usuario. Por favor, intente nuevamente.",
+                boton: "Intente nuevamente",
+                redireccion: "/autenticacion/registro"
+            });
+        });
+    } else {
+        // Mapeo de errores y datos anteriores del formulario
+        const mapeoErroes = errors.mapped();
+        const old = req.body;
+
+        // Renderizar vista con errores y datos anteriores
+        res.render("./authentication/registro", {
+            errors: mapeoErroes,
+            old: old,
+        });
+    }
+};
